d06c92f87f92061e109878df2427ef7dbb9692ca
Richard Zinecker <zinecker@miracode.io>
2023-11-15 15:11:02 +0100

    added DB connection

--- utils/login.php
+++ utils/login.php
@@ -1,8 +1,17 @@
 <?php
     // ToDo
+
+    // Get Username and Password from the Loginform and Hash the Password with sha256
     $user = $_POST["username"];
     $password = hash('sha256', $_POST["password"]);
-    echo sprintf("User: %s, Password Hash: %s", $user, $password);
+    echo sprintf("User: %s, Password Hash: %s <br>", $user, $password);
+
+    //create connection to DB
+    //works with that port, user and password on the PC of Zinni
+    //ToDo: get Values from .env
+    $db_connection = pg_connect("host=127.0.0.1 port=1337 user=postgres password=Hunter1337") or die("Could not connect");
+    echo "Connection successfully";
+    pg_close($db_connection);
 ?>
 <!-- <script>
     window.location.replace("/utils/me.php")
