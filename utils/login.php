6ae253e9195b13f24cd07f677f746132f7e0770f
Richard Zinecker <zinecker@miracode.io>
2023-11-15 23:27:38 +0100

    get .env Values working

--- utils/login.php
+++ utils/login.php
@@ -1,15 +1,19 @@
 <?php
     // ToDo
 
-    // Get Username and Password from the Loginform and Hash the Password with sha256
+    //loading the values of the .env File
+    $root = $_SERVER['DOCUMENT_ROOT'];
+    $envFilepath = "$root/.env";
+    $env = parse_ini_file($envFilepath);
+
+    //Get Username and Password from the Loginform and Hash the Password with sha256
     $user = $_POST["username"];
     $password = hash('sha256', $_POST["password"]);
     echo sprintf("User: %s, Password Hash: %s <br>", $user, $password);
 
     //create connection to DB
     //works with that port, user and password on the PC of Zinni
-    //ToDo: get Values from .env
-    $db_connection = pg_connect(sprintf("host=%s port=%s user=%s password=%s", $_ENV["Host"], $_ENV["Port"], $_ENV["User"], $_ENV["Password"])) or die("Could not connect");
+    $db_connection = pg_connect(sprintf("host=%s port=%s user=%s password=%s", $env["Host"], $env["Port"], $env["User"], $env["Password"])) or die("Could not connect");
     echo "Connection successfully";
     pg_close($db_connection);
 ?>
