76f6355af376140e61e8b81c029b686407ff67f8
Richard Zinecker <zinecker@miracode.io>
2023-11-16 19:30:56 +0100

    first layout of me.php #6

--- me.php
+++ me.php
@@ -8,6 +8,14 @@
         <title>elephriend. | me</title>
     </head>
     <body>
+        <?php 
+            $img="2";
+            $user="Richard";
+            $username="Zinni";
+            $birthday="23.09.2003";
+            $age="20";
+            $location="Leipzig";
+        ?>
         <header>
             <div class="wrapper header">
                 <img id="logo" src="utils/img/elephriend-2-ele.png" alt="elephriend-logo">
@@ -19,9 +27,57 @@
             </div>
         </header>
         <div class="wrapper content">
-            <?php 
-                echo "no connection to db"
-            ?>
+            <div class="me">
+                <div>
+                    <?php
+                        echo sprintf('<img id="bioImage" src="utils/img/bio/img_%s.jpg" alt="Bio Image">', $img);  
+                    ?>
+                </div>
+                <div class="headlines" >
+                    <div class="userBig" >
+                        <?php 
+                            echo $user;
+                        ?>
+                    </div>
+                    <div class="devider"></div> 
+                    <div class="usernameBig" >
+                        <?php 
+                            echo $username;
+                        ?>
+                    </div>
+                </div>
+            </div>
+            <div class="me">
+                <div class="datacard">
+                    <div class="dataHead">
+                        Geburstag:
+                    </div>
+                    <div class="dataEntry">
+                        <?php 
+                            echo $birthday;
+                        ?>
+                    </div>
+                    <div class="dataHead">
+                        Alter:
+                    </div>
+                    <div class="dataEntry">
+                        <?php 
+                            echo $age;
+                        ?>
+                    </div>
+                    <div class="dataHead">
+                        Standort:
+                    </div>
+                    <div class="dataEntry">
+                        <?php 
+                            echo $location;
+                        ?>
+                    </div>
+                </div>
+                <div>
+                    Bio
+                </div>
+            </div>
         </div>
         <footer>
             <div class="wrapper">
