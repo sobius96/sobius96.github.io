72de4da720166092deb67ddd57aca1bd196bdeb4
Richard Zinecker <zinecker@miracode.io>
2023-11-30 15:49:49 +0100

    show user #3

--- index.php
+++ index.php
@@ -37,7 +37,13 @@
             </div>
         </header>
         <div class="wrapper content">
-            <?php 
+            <?php
+                echo '<script>
+                        function showUser(id) {
+                            document.cookie = "friend=" + id + "; path=/";
+                            window.location.href = "/user.php";
+                        }
+                    </script>';
                 foreach($data as &$user) {
                     echo sprintf(
                         '<div class="userProfile">
@@ -48,13 +54,13 @@
                                     <div>%s</div>
                                 </div>
                             </div>
-                            <button class="userButton">
+                            <button class="userButton" onClick="showUser(%s)">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="heroicon" fill="none" viewBox="0 0 24 24" stroke="#000000" stroke-width="2">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                                 </svg>
                             </button>
-                        </div>', $user["profile_picture"], $user["username"], $user["user_location"]);
+                        </div>', $user["profile_picture"], $user["username"], $user["user_location"], $user["user_id"]);
                 }
                 unset($user);
             ?>
